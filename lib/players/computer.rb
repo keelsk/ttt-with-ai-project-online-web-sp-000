3f8a0fcae9e5b58dccb145b7dfa2fdc38944457e
Kirsten Keels <keelsk@gmail.com>
2020-03-11 03:04:32 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -5,22 +5,9 @@ module Players
       move = nil
       if !(board.taken?(5))
         move = "5"
-      elsif board.cells.uniq.length 
-        array = ["1", "3", "7", "9"]
+      else 
+        array = ["1", "2", "3", "4", "6", "7", "8", "9"]
         move = array.sample
-      elsif !(board.taken?(3)
-        move = "3"
-      elsif !(board.taken?(7))
-        move = "7"
-      elsif !(board.taken?(9))
-        move = "9"
-      else
-        array = [1, 2, 3, 4,5, 6, 7, 8, 9]
-        array.find_index do |x, index| 
-          if !(board.taken?(index))
-            move = "#{index}"
-          end
-        end
       end
       move
     end
