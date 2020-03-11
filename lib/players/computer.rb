2bd8d31ac0014c81a0f8257718490ceb6ea7484a
Kirsten Keels <keelsk@gmail.com>
2020-03-11 02:05:26 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -5,15 +5,12 @@ module Players
       move = nil
       if !(board.taken?(5))
         move = "5"
-      elsif board.turn_count == 3 || board.turn_count == 5
-        array = ["1", "3", "7", "9"]
+      elsif board.taken?(1)
+        array = ["2", "3", "4", "7"]
         move = array.sample
-        
-      else
-        array 
-        array.each do |index|
-          board.taken?(index)
-        end
+      elsif board.taken?(2)
+        array = ["3", "5", "8"]
+        move = array.sample
       end
     end
   end
