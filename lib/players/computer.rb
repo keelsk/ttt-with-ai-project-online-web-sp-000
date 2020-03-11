802c0d1a63b8ad30d5f166c41730a36fd759f272
Kirsten Keels <keelsk@gmail.com>
2020-03-11 02:22:46 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -20,18 +20,6 @@ module Players
       elsif board.taken?(5)
         array = ["1", "3", "7", "9"]
         move = array.sample
-      elsif board.taken?(6)
-        array = ["3", "4", "5", "9"]
-        move = array.sample
-      elsif board.taken?(7)
-        array = ["1", "3", "4", "5", "7", "8", "9"]
-        move = array.sample
-      elsif board.taken?(8)
-        array = ["2", "5", "7", "9"]
-        move = array.sample
-      elsif board.taken?(9)
-        array = ["1", "3", "5", "6", "7", "8"]
-        move = array.sample
       else
         array = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
         move = array.sample
