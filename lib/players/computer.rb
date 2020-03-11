84557b7c7c5a1fff2c0508cb2455ac794724d5e1
Kirsten Keels <keelsk@gmail.com>
2020-03-11 02:18:22 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -30,7 +30,7 @@ module Players
         array = ["2", "5", "7", "9"]
         move = array.sample
       elsif board.taken?(9)
-        array = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        array = ["1", "3", "5", "6", "7", "8"]
         move = array.sample
       end
       move
