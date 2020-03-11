729139dc290fb7d2bd8317c0513c1785aead1958
Kirsten Keels <keelsk@gmail.com>
2020-03-11 02:44:31 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -7,6 +7,12 @@ module Players
         move = "5"
       elsif !(board.taken?(1))
         move = "1"
+      elsif !(board.taken?(3)
+        move = "3"
+      elsif !(board.taken?(7))
+        move = "7"
+      elsif !(board.taken?(9))
+        move = "9"
       else
         array = [1, 2, 3, 4,5, 6, 7, 8, 9]
         array.find_index do |x, index| 
